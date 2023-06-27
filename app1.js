3e918a73baaed8d42cdd117fa2d40bb63103a590
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-06-27 10:24:06 +0100

    Previous JavaScript consigned to app1.js and new app.js created to continue working.

---
+++ app1.js
@@ -0,0 +1,113 @@
+"use strict";
+
+const allProducts = [];
+const productImage1 = document.querySelector("section img:nth-child(2)");
+const productImage2 = document.querySelector("section img:nth-child(3)");
+const productImage3 = document.querySelector("section img:nth-child(4)");
+const productSection = document.querySelector("section");
+const resultsButton = document.querySelector("button");
+const resultsList = document.querySelector("ul");
+
+let rounds = 0;
+const maxRounds = 5;
+
+productSection.addEventListener("click", handleProductClick);
+
+function Product(name) {
+  this.name = name;
+  this.imgUrl = `images/${name}.jpg`;
+  this.timesShown = 0;
+  this.timesClicked = 0;
+  allProducts.push(this);
+}
+
+function getRandomIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+function renderProducts() {
+  let product1Index = getRandomIndex();
+  let product2Index = getRandomIndex();
+  let product3Index = getRandomIndex();
+
+  //   console.log(product1Index, product2Index, product3Index);
+
+  while (
+    product1Index === product2Index ||
+    product1Index === product3Index ||
+    product2Index === product3Index
+  ) {
+    product2Index = getRandomIndex();
+    product3Index = getRandomIndex();
+  }
+
+  allProducts[product1Index].timesShown++;
+  allProducts[product2Index].timesShown++;
+  allProducts[product3Index].timesShown++;
+
+  //   console.log(product1Index, product2Index, product3Index);
+
+  productImage1.src = allProducts[product1Index].imgUrl;
+  productImage2.src = allProducts[product2Index].imgUrl;
+  productImage3.src = allProducts[product3Index].imgUrl;
+
+  productImage1.alt = allProducts[product1Index].name;
+  productImage2.alt = allProducts[product2Index].name;
+  productImage3.alt = allProducts[product3Index].name;
+}
+
+function handleProductClick(event) {
+  if (event.target.tagName === "IMG") {
+    rounds++;
+
+    let selectedProductName = event.target.alt;
+
+    for (let i = 0; i < allProducts.length; i++) {
+      if (allProducts[i].name === selectedProductName) {
+        allProducts[i].timesClicked++;
+      }
+    }
+
+    if (rounds === maxRounds) {
+      productSection.removeEventListener("click", handleProductClick);
+      productSection.classList.add("disable");
+      resultsButton.disabled = false;
+      resultsButton.addEventListener("click", displayResults);
+    }
+    renderProducts();
+  }
+  return;
+}
+
+// WORK ON THIS FUNCTION TOMORROW
+
+function displayResults() {
+  console.log("results");
+  for (let i = 0; i < allProducts.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = `${allProducts[i].name} was viewed ${allProducts[i].timesShown} and was selected ${allProducts[i].timesClicked} times.`;
+    resultsList.appendChild(li);
+  }
+}
+
+const bag = new Product("bag");
+const banana = new Product("banana");
+const bathroom = new Product("bathroom");
+const boots = new Product("boots");
+const breakfast = new Product("breakfast");
+const bubblegum = new Product("bubblegum");
+const chair = new Product("chair");
+const cthulhu = new Product("cthulhu");
+const dogDuck = new Product("dog-duck");
+const dragon = new Product("dragon");
+const pen = new Product("pen");
+const petSweep = new Product("pet-sweep");
+const scissors = new Product("scissors");
+const shark = new Product("shark");
+const sweep = new Product("sweep");
+const tauntaun = new Product("tauntaun");
+const unicorn = new Product("unicorn");
+const waterCan = new Product("water-can");
+const wineGlass = new Product("wine-glass");
+
+renderProducts();
